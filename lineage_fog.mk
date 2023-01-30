32bb7ce1534b2d35ce40de040336139fb1a161da
boedhack99 <boedhack@gmail.com>
2023-01-30 18:46:33 +0700

    AlphaDroid Now
    Change-Id: Ia349da47d603817812dfcbab8fbedf80f9aa51c3

--- lineage_fog.mk
+++ lineage_fog.mk
@@ -14,6 +14,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
+# Alpha internal properties
+ALPHA_MAINTAINER := ⓓⓑⓛⓔⓝⓚ
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 PRODUCT_NAME := lineage_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
@@ -23,3 +28,6 @@ PRODUCT_MODEL := Redmi 10C
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/fog_global/fog:12/SKQ1.211103.001/V13.0.2.0.SGEMIXM:user/release-keys
+
+# GAPPS
+WITH_GAPPS := true
